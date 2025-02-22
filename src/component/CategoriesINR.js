dc8200bc3e48b56690429adab052b971a5d736fa
Darkstrike <sayantandas579pc@gmail.com>
2025-02-22 18:29:12 +0530

    Update CategoriesINR.js

--- src/component/CategoriesINR.js
+++ src/component/CategoriesINR.js
@@ -22,8 +22,8 @@ class CategoriesINR extends React.Component {
                             <div className="card" style={{ width: '18rem' }}>
                                 <img src="https://via.placeholder.com/150" className="card-img-top" alt="..." />
                                 <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <h5 className="card-title">BASIC RIGHTS</h5>
+                                    <p className="card-text">Click on it for knowing the basic rights.</p>
                                     <a href="#" className="card-link">Card link</a>
                                     <a href="#" className="card-link">Another link</a>
                                 </div>
