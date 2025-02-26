f5418e7b4da8cdfe9ea2bafb6a85cb1f2a1ecfbf
Darkstrike <sayantandas579pc@gmail.com>
2025-02-26 19:37:12 +0530

    Update CategoriesINR.js

--- src/component/CategoriesINR.js
+++ src/component/CategoriesINR.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 class CategoriesINR extends React.Component {
     render() {
         return (
             <>
                 <nav style={{ '--bs-breadcrumb-divider': '">"' }} aria-label="breadcrumb">
-                    <ol className="breadcrumb">
+                    <ol className="breadcrumb container mt-4">
                         <li className="breadcrumb-item"><a href="./Library">Library</a></li>
                         <li className="breadcrumb-item active" aria-current="page">Indian Constituion</li>
                     </ol>
@@ -17,27 +18,28 @@ class CategoriesINR extends React.Component {
                 </div>
                 <div className="container mt-4">
                     <div className="row">
-                        {/* First Card */}
-                        <div className="col-md-4 mb-4">
-                            <div className="card" style={{ width: '18rem' }}>
-                                <img src="https://via.placeholder.com/150" className="card-img-top" alt="..." />
-                                <div className="card-body">
-                                    <h5 className="card-title">BASIC RIGHTS</h5>
-                                    <p className="card-text">Click on it for knowing the basic rights.</p>
-                                    <Link to="/category/basic-rights" className="card-link">Come..</Link>
-                                    <a href="#" className="card-link">Another link</a>
-                                </div>
-                            </div>
-                        </div>
+
 
                         {/* Second Card */}
                         <div className="col-md-4 mb-4">
                             <div className="card" style={{ width: '18rem' }}>
                                 <img src="https://via.placeholder.com/150" className="card-img-top" alt="..." />
                                 <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" className="card-link">Card link</a>
+                                    <h5 className="card-title">Constituional Rights</h5>
+                                    <p className="card-text">The rights that people of india have</p>
+                                    <Link to="/category/domestic-rights" className="card-link">Come..</Link>
+                                    <a href="#" className="card-link">Another link</a>
+                                </div>
+                            </div>
+                        </div>
+                                                {/* First Card */}
+                                                <div className="col-md-4 mb-4">
+                            <div className="card" style={{ width: '18rem' }}>
+                                <img src="https://via.placeholder.com/150" className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">BASIC RIGHTS</h5>
+                                    <p className="card-text">Click on it for knowing the basic rights.</p>
+                                    <Link to="/category/basic-rights" className="card-link">Come..</Link>
                                     <a href="#" className="card-link">Another link</a>
                                 </div>
                             </div>
