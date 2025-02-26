b8dfc35a648775eee84b708652079cbf0bb60be3
Darkstrike <sayantandas579pc@gmail.com>
2025-02-26 19:36:34 +0530

    Update Library.js

--- src/component/Library.js
+++ src/component/Library.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Library extends React.Component {
     render() {
         return (
-            <div>
+            <div className='container mt-4'>
                 <h1>Library Page</h1>
                 {/* Add your library content here */}
             </div>
