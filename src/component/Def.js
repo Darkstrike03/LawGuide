25ad092b1a322c409170b340cfc6de29e2f855d8
Darkstrike <sayantandas579pc@gmail.com>
2025-03-07 22:16:49 +0530

    Add files via upload

---
+++ src/component/Def.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Def extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Library Page</h1>
+                {/* Add your library content here */}
+            </div>
+        );
+    }
+}
+
+export default Def;
