2e2bf5da763c7503387a80584dda80d5421f6421
Darkstrike <sayantandas579pc@gmail.com>
2025-03-07 22:14:54 +0530

    Add files via upload

---
+++ src/component/Ndefault.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Fo from './assets/oops.png';
+import './Ndefault.css'; // Import the CSS file for additional styling
+
+class Ndefault extends React.Component {
+    render() {
+        return (
+            <div className="d-flex justify-content-center align-items-center vh-100">
+                <div className="card text-center" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)', border: 'none' }}>
+                    <div className="card-body">
+                        <h1 className="card-title">No Notices Available</h1>
+                        <img 
+                            src={Fo}   
+                            className="card-img-top" 
+                            alt="Background" 
+                            style={{ maxWidth: '100%', height: 'auto' }}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Ndefault;
