b647edbc4ddabe3102d012a05c20a20a35d312e7
Darkstrike <sayantandas579pc@gmail.com>
2025-03-08 16:52:56 +0530

    Update Header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../logo.png';
+import { Link } from 'react-router-dom';
+import { supabase } from '../supabaseClient';
 
-class Header extends React.Component{
-    render(){
-        return (
-            <nav className="navbar" style={{ backgroundColor: '#343a40' }}>
-        <div className="container-fluid">
-          <a className="navbar-brand d-flex align-items-center" href="#"style={{ color: 'white' }}>
-            <img src={logo} alt="Logo" width={50} height={50} className="d-inline-block align-text-top" />
-            <span style={{ fontFamily: 'Edwardian Script ITC', fontSize: '1.5em' }}>L</span>AW GUIDE
-          </a>
-        </div>
-      </nav>
-        );
-    }
-}
+const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [profilePicture, setProfilePicture] = useState("");
+
+  useEffect(() => {
+    const checkUser = async () => {
+      const { data: { user } } = await supabase.auth.getUser();
+      if (user) {
+        setIsLoggedIn(true);
+        setProfilePicture(user.user_metadata?.profile_picture || "default1.png");
+      }
+    };
+    checkUser();
+  }, []);
+
+  return (
+    <nav className="navbar" style={{ backgroundColor: '#343a40' }}>
+      <div className="container-fluid d-flex justify-content-between align-items-center">
+        <a className="navbar-brand d-flex align-items-center" href="#" style={{ color: 'white' }}>
+          <img src={logo} alt="Logo" width={50} height={50} className="d-inline-block align-text-top" />
+          <span style={{ fontFamily: 'Edwardian Script ITC', fontSize: '1.5em' }}>L</span>AW GUIDE
+        </a>
+        <button className="btn btn-outline-light" type="button">
+          {isLoggedIn ? (
+            <Link to="/profile" style={{ color: 'white' }}>
+              <img
+                src={`/ppic/${profilePicture}`}
+                alt="Profile"
+                width={30}
+                height={30}
+                style={{ borderRadius: '50%' }}
+              />
+            </Link>
+          ) : (
+            <Link to="/login" style={{ color: 'white' }}>Login</Link>
+          )}
+        </button>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
