6873b82fb2fa94ec9df939347f52e2dc4656d35a
Darkstrike <sayantandas579pc@gmail.com>
2025-04-28 20:15:27 +0530

    Update Container4.js

--- src/component/Container4.js
+++ src/component/Container4.js
@@ -5,7 +5,7 @@ class Container extends React.Component {
         return (
             <div className="container mt-4">
                 <h4>Disclaimer:</h4>
-                <p>The content provided on this website is for informational purposes only and should not be relied upon as a substitute for legal advice. Recipients are advised to consult with qualified legal counsel before implementing any recommendations herein. LawConnect shall not be liable for actions taken based on this information.</p>
+                <p>The content provided on this website is for informational purposes only and should not be relied upon as a substitute for legal advice. Recipients are advised to consult with qualified legal counsel before implementing any recommendations herein. Law Guide shall not be liable for actions taken based on this information.</p>
                 <p>* Please note that if you choose to engage with a lawyer, they may charge fees for their services.</p>
             </div>
         );
