87adb5cbc20bc3887b612b6f12c8237bd58d74f1
Darkstrike <sayantandas579pc@gmail.com>
2025-04-28 20:27:24 +0530

    Update Card1.js

--- src/component/Card1.js
+++ src/component/Card1.js
@@ -18,7 +18,7 @@ class Card1 extends React.Component {
                         </p>
                         <div className="d-flex justify-content-center my-4">
                             <a href="#" className="card1-btn">
-                                <span className="me-2"><Link to="/library">Learn More</Link></span>
+                                <span className="me-2"><Link className="me-2" to="/library">Learn More</Link></span>
                                 <svg fill="#ffffff" height="24px" width="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M0 0h24v24H0z" fill="none"/>
                                     <path d="M12 4v1.79c-3.39.49-6 3.39-6 6.91s2.61 6.42 6 6.91V20c-4.42-.5-8-4.25-8-8.5S7.58 4.5 12 4zm0 2.5c-3.04 0-5.5 2.46-5.5 5.5s2.46 5.5 5.5 5.5 5.5-2.46 5.5-5.5S15.04 6.5 12 6.5zm1 5.5h-2v2h2v-2zm0-4h-2v2h2V8z"/>
