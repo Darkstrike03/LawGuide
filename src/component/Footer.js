efa3ec3249e29e22cda5a85389e9ac2403be5ba2
Darkstrike <sayantandas579pc@gmail.com>
2025-04-29 12:13:57 +0530

    Update Footer.js

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -8,9 +8,10 @@ class Footer extends React.Component {
                 <div className="container-fluid text-center">
                     <p>&copy; {new Date().getFullYear()} LAW GUIDE. All rights reserved.</p>
                     <ul className="list-inline">
-                        <li className="list-inline-item"><Link to="/PPol" style={{ color: 'white' }}>Privacy Policy</Link></li>
-                        <li className="list-inline-item"><a href="#" style={{ color: 'white' }}>Terms of Service</a></li>
-                        <li className="list-inline-item"><a href="#" style={{ color: 'white' }}>Contact Us</a></li>
+                        <li className="list-inline-item"><a href="/privacy-policy" style={{ color: 'white' }}>Privacy Policy</a></li>
+                        <li className="list-inline-item"><a href="/terms-of-service" style={{ color: 'white' }}>Terms of Service</a></li>
+                        <li className="list-inline-item"><a href="https://mail.google.com/mail/?view=cm&fs=1&to=etherealarchives017@gmail.com&su=Contact+Us&body=Hello%2C+I+have+a+question+about+your+website." 
+       target="_blank" style={{ color: 'white' }}>Contact Us</a></li>
                     </ul>
                 </div>
             </footer>
