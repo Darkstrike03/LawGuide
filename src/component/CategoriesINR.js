69cacc9d7dc2dde0a21983107937553108beef97
Darkstrike <sayantandas579pc@gmail.com>
2025-05-02 15:31:44 +0530

    Update CategoriesINR.js

--- src/component/CategoriesINR.js
+++ src/component/CategoriesINR.js
@@ -24,7 +24,6 @@ class CategoriesINR extends React.Component {
                         {/* Second Card */}
                         <div className="col-md-4 mb-4">
                             <div className="card" style={{ width: '18rem' }}>
-                                <img src="https://via.placeholder.com/150" className="card-img-top" alt="..." />
                                 <div className="card-body">
                                     <h5 className="card-title">Constituional Rights</h5>
                                     <p className="card-text">The rights that people of india have</p>
@@ -36,7 +35,7 @@ class CategoriesINR extends React.Component {
                                                 {/* First Card */}
                                                 <div className="col-md-4 mb-4">
                             <div className="card" style={{ width: '18rem' }}>
-                                <img src="https://via.placeholder.com/150" className="card-img-top" alt="..." />
+                                
                                 <div className="card-body">
                                     <h5 className="card-title">BASIC RIGHTS</h5>
                                     <p className="card-text">Click on it for knowing the basic rights.</p>
@@ -49,7 +48,7 @@ class CategoriesINR extends React.Component {
                         {/* Third Card */}
                         <div className="col-md-4 mb-4">
                             <div className="card" style={{ width: '18rem' }}>
-                                <img src="https://via.placeholder.com/150" className="card-img-top" alt="..." />
+                                
                                 <div className="card-body">
                                     <h5 className="card-title">Fundamental Rights</h5>
                                     <p className="card-text">check out the fundamental rights</p>
@@ -62,7 +61,7 @@ class CategoriesINR extends React.Component {
                         {/* Fourth Card */}
                         <div className="col-md-4 mb-4">
                             <div className="card" style={{ width: '18rem' }}>
-                                <img src="https://via.placeholder.com/150" className="card-img-top" alt="..." />
+                                
                                 <div className="card-body">
                                     <h5 className="card-title">Card title</h5>
                                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
